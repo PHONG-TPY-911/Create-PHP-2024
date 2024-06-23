431c09c8527504c1cf37bb998214f30dda04531d
kanya xayyahan <kanyaxayyahan@kanyas-MacBook-Pro.local>
2024-06-23 13:46:03 +0700

    Committing changes before switching to main branch

--- connect-database/config.php
+++ connect-database/config.php
@@ -1,15 +1,14 @@
-<?php 
-
-$servername = "localhost";
+<?php
+$servername = "127.0.0.1";
 $username = "root";
-$password = "";
+$password = ""; // Use the password you set during the mysql_secure_installation step
+$dbname = "database_final";
 
-try{
-    $conn = new PDO("mysql:host=$servername; dbname=database_final", $username, $password);
-    // $conn = new PDO("mysql:host=$servername;dbname=final", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-    echo "Connection Database Failed : " . $e->getMessage();
+    echo "Connection successful!";
+} catch (PDOException $e) {
+    echo "Connection Database Failed: " . $e->getMessage();
 }
-
 ?>
